720e93b00bae209e1d923715c497ae53b04b2d24
JAMIE KRISTINA ZOILON <20200019675@my.xu.edu.ph>
2025-06-12 22:55:55 +0800

    Update auth.routes.js

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controller');
+ const User = require('../models/User');
+  const bcrypt = require('bcrypt');
+  const hashedPassword = await bcrypt.hash("password123", 10);
+
+  const testUser = new User({
+    name: "jam",
+    email: "zoilonjam@gmail.com",
+    department: "social services",
+    username: "jam",
+    password: hashedPassword
+  });
+
+  try {
+    await testUser.save();
+    res.send("Test user created.");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Failed to create test user.");
+  }
+});
+
 
 router.post('/signup', userController.signup);
 router.post('/login', userController.login);
-
+router.get('/create-test-user', async (req, res) => {
+ 
 module.exports = router;
