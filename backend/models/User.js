24a1c688236bcb01bbeb663b8a3a0af65c931f6e
JAMIE KRISTINA ZOILON <20200019675@my.xu.edu.ph>
2025-06-22 22:27:43 +0800

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  department: { type: String, required: true },
+  name: String,
+  email: String,
+  department: String,
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  phone: String,
+  role: {
+    type: String,
+    enum: ['admin', 'employee'], 
+    default: 'employee' 
+  }
 });
 
 module.exports = mongoose.model('User', userSchema);
+
